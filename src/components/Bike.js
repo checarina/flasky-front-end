ca6685e3cc1cdfdeca5c8bb5c6b935218f531ca4
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-12-13 11:08:05 -0800

    Accept props in bike

--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,26 +1,30 @@
+import PropTypes from 'prop-types'
+
 import "./Bike.css";
 
-function Bike(){
-    const myBike = {
-        id: 1,
-        name: "My bike to be rendered",
-        size: 50,
-        price: 100,
-        type: "special"
-    };
+function Bike( { id, name, size, type, price } ){
+
+    const bikeName = name;
+    const bikeId = id;
+    const bikeSize = size;
+    const bikeType = type;
+    const bikePrice = price;
 
     return(
         <div>
-            <h2 className="bike__name">{myBike.name}</h2>
+            <h2 className="bike__name">{bikeName}</h2>
             <ul>
-                <li>{myBike.id}</li>
-                <li>{myBike.size}</li>
-                <li>{myBike.price}</li>
-                <li>{myBike.type}</li>
+                <li>{bikeId}</li>
+                <li>{bikeSize}</li>
+                <li>{bikeType}</li>
+                <li>{bikePrice}</li>
             </ul>
             
         </div>
     );
 }
 
+Bike.propTypes = {
+    id: PropTypes.number.isRequired,
+}
 export default Bike;
