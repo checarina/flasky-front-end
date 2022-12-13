f66f5c60a27dd7677f125ba36a04c87e121b84cd
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-12-13 11:08:55 -0800

    Add a bike components array in bikelist

--- src/components/BikeList.js
+++ src/components/BikeList.js
@@ -1,26 +1,42 @@
+import PropTypes from 'prop-types'
+
 import Bike from "./Bike";
 
-function BikeList(){
- const bikesList = [
-    {
-        id: 1,
-        name: "This is some data that we will work to display next class!",
-        size: 50,
-        price: 100,
-        type: "special"
-    },
-    {
-        id: 2,
-        name: "This is some data that we will work to display next class!",
-        size: 40,
-        price: 20,
-        type: "duplicate"
+function BikeList( { bikesList } ){
+    const bikeComponents = [];
+
+    for (const bike of bikesList) {
+        bikeComponents.push(
+            <li key={bike.id}>
+                <Bike 
+                    id={bike.id} 
+                    name={bike.name} 
+                    size={bike.size} 
+                    price={bike.price} 
+                    type={bike.type}
+                />
+            </li>
+        )
     }
- ];
 
  return(
-    bikesList.map((bike) => <Bike/>)
+    <div>
+        {bikeComponents}
+    </div>
     //returns [<Bike/>, <Bike/>]
  );
 }
+
+BikeList.propTypes = {
+    bikesList: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            size: PropTypes.number.isRequired,
+            type: PropTypes.string.isRequired,
+            price: PropTypes.string.isRequired,
+        })
+    ),
+}
+
 export default BikeList;
