5da23e3ad7f9702c693f0941cf36062b3c5de686
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-12-13 11:09:08 -0800

    Add unique id

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css"
 function Navbar(){
     const links=["Home", "Add Bike", "Edit Bike"];
     return(
-        <nav>{links.map((link) => <a href="http://www.adadevelopersacademy.org">{link}</a>)}</nav>
+        <nav>{links.map((link) => <a key={link} href="http://www.adadevelopersacademy.org">{link}</a>)}</nav>
     )
 
 }
