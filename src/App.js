743282e289195c03ca638a09f84f87f1376b5941
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-12-13 11:07:39 -0800

    Move bikeList to App / Add new bike

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import Navbar from './components/Navbar';
 import BikeList from './components/BikeList.js';
 
 function App() {
+
+  const bikesList = [
+    {
+        id: 1,
+        name: "This is some data that we will work to display next class!",
+        size: 50,
+        price: 100,
+        type: "special"
+    },
+    {
+        id: 2,
+        name: "This is some data that we will work to display next class!",
+        size: 40,
+        price: 20,
+        type: "duplicate"
+    },
+    {
+        id: 3,
+        name: "My new bike!!",
+        size: 80,
+        price: 100,
+        type: "unique"
+    }
+ ];
+
   return (
     <div>
         <Navbar/>
-        <BikeList/>
+        <BikeList bikesList={bikesList} />
         <button>Add Bike</button>
     </div>
   );
