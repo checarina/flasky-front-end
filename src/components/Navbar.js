e13786063dfb485b1ea7733d02acacc6c910e8af
Jerica Huang <jerica.huang@gmail.com>
2022-12-13 16:03:23 -0800

    navlink styling convention

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css"
 function Navbar(){
     const links=["Home", "Add Bike", "Edit Bike"];
     return(
-        <nav>{links.map((link) => <a key={link} href="http://www.adadevelopersacademy.org">{link}</a>)}</nav>
+        <nav>{links.map((link) => <a className="navbar__link" key={link} href="http://www.adadevelopersacademy.org">{link}</a>)}</nav>
     )
 
 }
